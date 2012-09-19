df5db6ae714e5a30a88614c390dfd18b33bede48
Teafrog <zukodina@gmail.com>
2012-09-19 15:20:29 +0200

    Database is working now! You serch and find runan and its drown on the map

--- src/com/chalmers/schmaps/GoogleMapActivity.java
+++ src/com/chalmers/schmaps/GoogleMapActivity.java
@@ -3,6 +3,7 @@ package com.chalmers.schmaps;
 import java.util.HashMap;
 import java.util.List;
 
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
@@ -10,10 +11,12 @@ import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
@@ -22,36 +25,117 @@ import android.widget.EditText;
 
 public class GoogleMapActivity extends MapActivity {
 
-    	private LocationManager location_manager;
+	private LocationManager location_manager;
 	private LocationListener location_listener;
 	private List<Overlay> mapOverlays;
 	private MapItemizedOverlay overlay;
 
-    	@Override
-    	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	setContentView(R.layout.activity_map); //en xml får skapas med det namnet, ändra namnet annars
-	MapView mapView = (MapView) findViewById(R.id.mapview);
-	mapView.setBuiltInZoomControls(true);
-	mapView.setSatellite(true);
+		setContentView(R.layout.activity_map); //en xml får skapas med det namnet, ändra namnet annars
+		MapView mapView = (MapView) findViewById(R.id.mapview);
+		mapView.setBuiltInZoomControls(true);
+		mapView.setSatellite(true);
 
-	mapOverlays = mapView.getOverlays();
-	Drawable drawable = this.getResources().getDrawable(R.drawable.ic_launcher); 
-	//ic_launcer är bilden jag har använt, välj en annan bild och lägg i drawable och referera till den
+		mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(R.drawable.ic_launcher); 
+		overlay = new MapItemizedOverlay(drawable, this);
 
-	overlay = new MapItemizedOverlay(drawable, this);
+		SearchSQL search = new SearchSQL(GoogleMapActivity.this);
+		search.openWrite(); //öppnar databasen för att skriva i den, denna kodsnutt ska inte vara här sen!
+		search.createEntry();
+		search.close(); 
 
-	GeoPoint gp = new GeoPoint(57689262,11973805); //skapar en geopunkt med följande latitude och logitue
 		
-		
-	String s1 = "Chalmersplatsen 1";
-	String s2 = "Våning 2";
-	OverlayItem over = new OverlayItem(gp, s1, s2); //s1 och s2 visas i dialogrutan
-	overlay.addOverlay(over);
-	mapOverlays.add(overlay);
+		search.openRead(); //öppnar databasen för läsafrån den
+		GeoPoint gp = new GeoPoint(search.getLat("Runan"),search.getLong("Runan")); //hämtar latitude och longitude i databasen och skapar en geopunkt 
 
-        /* detta är simons kod, markerar bort den än så länge
+		String s1 = search.getAddress("Runan");
+		String s2 = search.getLevel("Runan");
+		search.close();
+		OverlayItem over = new OverlayItem(gp, s1, s2); //s1 och s2 visas i dialogrutan
+
+		overlay.addOverlay(over);
+		mapOverlays.add(overlay);
+
+		location_manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		location_listener = new LocationListener(){
+
+			public void onLocationChanged(Location location) { //metod som hämtar din position genom att anropa onResume
+
+				/*
+				int longitude = (int) (location.getLongitude() * 1E6);
+				int latitude = (int) (location.getLatitude() * 1E6);
+
+				GeoPoint point = new GeoPoint(latitude, longitude);
+				OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+				overlay.addOverlay(overlayitem);
+				mapOverlays.add(overlay);
+				 */
+
+			}
+
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+
+			}
+
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+
+			}	
+		};
+
+	}
+
+
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_map, menu);
+		return true;
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		location_manager.removeUpdates(location_listener);
+	}
+
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		try {
+			// Register the listener with the Location Manager to receive
+			// location updates
+			location_manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 100, location_listener);
+			location_manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 30000, 100, location_listener);
+		}
+		catch (Exception e) {
+			//print("Couldn't use the GPS: " + e + ", " + e.getMessage());
+		}
+	}
+
+	/* detta är simons kod, markerar bort den än så länge
                 assignInstances();
     }
 
@@ -64,7 +148,7 @@ public class GoogleMapActivity extends MapActivity {
         showLecture = (TextView) findViewById(R.id.showLectureText);
         showLecture.setText("Hi!");
 	}
-	
+
 		public void onClick(View v) {
 		String lectureText = lectureEdit.getText().toString();
 		if(lectureHashMap.containsKey(lectureText))
@@ -76,32 +160,7 @@ public class GoogleMapActivity extends MapActivity {
 
 	}
 
-*/
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_map, menu);
-        return true;
-    }
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	
-	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();;
-	}
-
-
-
-	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-	}
+	 */
 }
+
+
--- src/com/chalmers/schmaps/SearchSQL.java
+++ src/com/chalmers/schmaps/SearchSQL.java
@@ -16,8 +16,8 @@ public class SearchSQL {
 	public static final String KEY_STREET = "street_name";
 	public static final String KEY_LEVEL = "level";
 	
-	private static final String DATABASE_NAME = "Schmapsdb"; //namnet på vår databas
-	private static final String DATABASE_TABLE = "RoomTable"; //namnet på vår tabell (kan ha flera tabeller)
+	private static final String DATABASE_NAME = "SchmapsDB"; //namnet på vår databas
+	private static final String DATABASE_TABLE = "SchmapsTable"; //namnet på vår tabell (kan ha flera tabeller)
 	private static final int DATABASE_VERSION = 1;
 	
 	private MySQLiteOpenHelper ourHelper;
@@ -25,34 +25,43 @@ public class SearchSQL {
 	private SQLiteDatabase ourDatabase;
 	
 	public SearchSQL(Context c){
-		ourContext = c;
-		
+		ourContext = c;	
 	}
 	
-	public SearchSQL open(){
+	public SearchSQL openWrite(){
 		ourHelper = new MySQLiteOpenHelper(ourContext);
 		ourDatabase = ourHelper.getWritableDatabase();
+		return this;
+	}
+	
+	public SearchSQL openRead(){
+		ourHelper = new MySQLiteOpenHelper(ourContext);
+		ourDatabase = ourHelper.getReadableDatabase();
 		return this;
 	}
 	
 	public void createEntry(){ //ska lägga in med den här metoden
 		ContentValues cv = new ContentValues();
 		
-		/* ett exempel på hur man lägger in
+		// ett exempel på hur man lägger in
+		cv.put(KEY_ROOM, "Runan");
+		cv.put(KEY_LAT, 57689262);
+		cv.put(KEY_LONG, 11973805);
+		cv.put(KEY_STREET, "Chalmersplatsen 1");
+		cv.put(KEY_LEVEL, "Plan 2");
+		ourDatabase.insert(DATABASE_TABLE, null, cv);
+		
 		cv.put(KEY_ROOM, "Hej");
 		cv.put(KEY_LAT, 58070517);
 		cv.put(KEY_LONG, 11760864);
-		cv.put(KEY_STREET, "Chalmersplatsen 2");
+		cv.put(KEY_STREET, "Gata 2");
 		cv.put(KEY_LEVEL, "Plan 4");
 		ourDatabase.insert(DATABASE_TABLE, null, cv);
-		*/ 
+		
 		// tips på hur man kan mata in från fil http://stackoverflow.com/questions/8801423/how-can-i-insert-data-in-an-sqlite-table-from-a-text-file-in-android
 		
 	}
-	
-	
-	
-	
+
 	public void close(){ 
 		ourHelper.close();
 	}
@@ -69,7 +78,7 @@ public class SearchSQL {
 			// TODO Auto-generated method stub
 			db.execSQL("CREATE TABLE " + DATABASE_TABLE + " (" +
 					KEY_ROWID + " INTEGER AUTOINCREMENT, " +
-					KEY_ROOM + " TEXT PRIMARY KEY, " +
+					KEY_ROOM + " TEXT NOT NULL, " +
 					KEY_LAT + " INTEGER, " + 
 					KEY_LONG + " INTEGER, " +
 					KEY_STREET + " TEXT NOT NULL, " +
@@ -86,6 +95,71 @@ public class SearchSQL {
 			
 		}
 		
+	}
+
+	public int getLat(String query) {
+		// TODO Auto-generated method stub
+		String [] columns = new String []{ KEY_ROWID, KEY_ROOM, KEY_LAT, KEY_LONG, KEY_STREET, KEY_LEVEL};
+		Cursor cursor = ourDatabase.query(DATABASE_TABLE, columns, KEY_ROOM + " LIKE ?" , new String [] { "%" + query + "%"}, null, null, null);
+		
+		if(cursor != null){
+			cursor.moveToFirst();
+			int lat = cursor.getInt(2);
+			if (cursor != null && !cursor.isClosed()) {
+	            cursor.close();
+			}
+			return lat;
+		}
+		return 0;
+	
+	}
+
+	public int getLong(String query) {
+		// TODO Auto-generated method stub
+		String [] columns = new String []{KEY_ROWID, KEY_ROOM, KEY_LAT, KEY_LONG, KEY_STREET, KEY_LEVEL };
+		Cursor cursor = ourDatabase.query(DATABASE_TABLE, columns, KEY_ROOM + " LIKE ?" , new String [] { "%" + query + "%"}, null, null, null);
+		
+		if(cursor != null){
+			cursor.moveToFirst();
+			int lon = cursor.getInt(3);
+			if (cursor != null && !cursor.isClosed()) {
+	            cursor.close();
+			}
+			return lon;
+		}
+		return 0;
+	}
+
+	public String getAddress(String query) {
+		// TODO Auto-generated method stub
+		String [] columns = new String []{KEY_ROWID, KEY_ROOM, KEY_LAT, KEY_LONG, KEY_STREET, KEY_LEVEL };
+		Cursor cursor = ourDatabase.query(DATABASE_TABLE, columns, KEY_ROOM + " LIKE ?" , new String [] { "%" + query + "%"}, null, null, null);
+		
+		if(cursor != null){
+			cursor.moveToFirst();
+			String address = cursor.getString(4);
+			if (cursor != null && !cursor.isClosed()) {
+	            cursor.close();
+			}
+			return address;
+		}
+		return null;
+	}
+
+	public String getLevel(String query) {
+		// TODO Auto-generated method stub
+		String [] columns = new String []{KEY_ROWID, KEY_ROOM, KEY_LAT, KEY_LONG, KEY_STREET, KEY_LEVEL };
+		Cursor cursor = ourDatabase.query(DATABASE_TABLE, columns, KEY_ROOM + " LIKE ?" , new String [] { "%" + query + "%"}, null, null, null);
+		
+		if(cursor != null){
+			cursor.moveToFirst();
+			String level = cursor.getString(5);
+			if (cursor != null && !cursor.isClosed()) {
+	            cursor.close();
+			}
+			return level;
+		}
+		return null;
 	}
 	
 }
