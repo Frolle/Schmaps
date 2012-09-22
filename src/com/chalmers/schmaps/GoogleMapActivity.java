856d7a66e8bd80da9abe72e8d801907949446b34
Martin Augustsson <mckan91@gmail.com>
2012-09-22 15:06:23 +0200

    Added some comments and that db is created at start of GoogleMapActivity

--- src/com/chalmers/schmaps/GoogleMapActivity.java
+++ src/com/chalmers/schmaps/GoogleMapActivity.java
@@ -14,6 +14,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Dialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class GoogleMapActivity extends MapActivity implements View.OnClickListener {
+	
+	private static String TAG = "GoogleMapActivity";
 	
 	private Button editButton;
     private EditText lectureEdit;
@@ -32,6 +35,7 @@ public class GoogleMapActivity extends MapActivity implements View.OnClickListen
 	private MapItemizedOverlay overlay;
 	private String roomToFind;
 	private MapView mapView;
+	private SearchSQL search;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -45,13 +49,10 @@ public class GoogleMapActivity extends MapActivity implements View.OnClickListen
 		mapOverlays = mapView.getOverlays();
 		Drawable drawable = this.getResources().getDrawable(R.drawable.ic_launcher); 
 		overlay = new MapItemizedOverlay(drawable, this);
-
-		SearchSQL search = new SearchSQL(GoogleMapActivity.this);
-		search.openWrite(); //öppnar databasen för att skriva i den, denna kodsnutt ska inte vara här sen!
-//		search.createEntry();
-		search.close(); 
-		
+	
 		assignInstances();
+		
+		
 
 		location_manager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 		location_listener = new LocationListener(){
@@ -85,6 +86,9 @@ public class GoogleMapActivity extends MapActivity implements View.OnClickListen
 
 			}	
 		};
+		
+		search = new SearchSQL(GoogleMapActivity.this);
+		search.createDatabase();
 
 	}
 
@@ -136,7 +140,7 @@ public class GoogleMapActivity extends MapActivity implements View.OnClickListen
 		roomToFind = lectureEdit.getText().toString();
 		roomToFind.toLowerCase().trim(); //removes white signs and converts to lower case
 		roomToFind = roomToFind.replaceAll("[^a-zA-Z0-9]+",""); //Removes illegal characters to prevent sql injection
-		SearchSQL search = new SearchSQL(GoogleMapActivity.this);
+		search = new SearchSQL(GoogleMapActivity.this);
 		search.openRead(); //öppnar databasen för läsafrån den
 		 if(search.exists(roomToFind)){
 			 GeoPoint gp = new GeoPoint(search.getLat(roomToFind),search.getLong(roomToFind)); //create a geopoint
