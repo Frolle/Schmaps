6b2c6399aaffd4fb3280a576a3bdbade96ae7ab4
Frolle <frollix@razzan.net>
2012-09-22 09:36:54 +0200

    Added some comments for javadoc

--- src/com/chalmers/schmaps/MenuActivity.java
+++ src/com/chalmers/schmaps/MenuActivity.java
@@ -6,7 +6,12 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-
+/**
+ * An activity that functions as a menu, it implements OnClickListener for assigning actions to buttons when clicked.
+ * 
+ * @author Frolle
+ *
+ */
 public class MenuActivity extends Activity implements View.OnClickListener {
 	Button searchHall;
     @Override
@@ -15,7 +20,9 @@ public class MenuActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_menu);
         assignInstances();
     }
-
+    /**
+     * Simple method to assign instance variables.
+     */
     private void assignInstances() {
         searchHall = (Button) findViewById(R.id.hButton);
         searchHall.setOnClickListener(this);
@@ -27,7 +34,9 @@ public class MenuActivity extends Activity implements View.OnClickListener {
         getMenuInflater().inflate(R.menu.activity_menu, menu);
         return true;
     }
-
+	/**
+	 * Determine what the buttons on the menu will do on click.
+	 */
 	public void onClick(View v) {
 		Intent startMapActivity = new Intent("android.intent.action.GOOGLEMAPACTIVITY");
 		startActivity(startMapActivity);		
