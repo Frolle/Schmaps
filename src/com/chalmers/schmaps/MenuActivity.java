d093b4e5327c4b1fc029fdb2414fb0bb1583e33a
Frolle <frollix@razzan.net>
2012-09-30 21:41:58 +0200

    Changed so that the extra info to next activity is a boolean for better optimizing

--- src/com/chalmers/schmaps/MenuActivity.java
+++ src/com/chalmers/schmaps/MenuActivity.java
@@ -51,7 +51,7 @@ public class MenuActivity extends Activity implements View.OnClickListener {
 		case R.id.searchHallButton:
 			searchHall.getBackground().setColorFilter(new LightingColorFilter(0x000000, 0x4682b4)); //graphics for the button
 			startMapActivity = new Intent("android.intent.action.GOOGLEMAPACTIVITY");
-			startMapActivity.putExtra("Show searchfield", LECTUREHALLBUTTON);
+			startMapActivity.putExtra("Show searchfield", true);
 			startActivity(startMapActivity);	
 			break;
 		
