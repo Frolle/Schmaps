2cd5476418c81c77776cf048d385f9e0da51737b
Kyathil <kyawolfwritten@gmail.com>
2012-10-03 15:58:42 +0200

    created new Activity for the new layout

---
+++ src/com/chalmers/schmaps/CheckoutMenu.java
@@ -0,0 +1,13 @@
+package com.chalmers.schmaps;
+
+import android.app.Activity;
+import android.view.View;
+
+public class CheckoutMenu extends Activity implements View.OnClickListener {
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
