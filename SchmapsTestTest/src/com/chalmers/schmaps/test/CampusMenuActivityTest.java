632a6080baf103d117e162eafb9ab238f548ec87
Frolle <frollix@razzan.net>
2012-10-08 15:20:29 +0200

    Added class skeleton for next test

---
+++ SchmapsTestTest/src/com/chalmers/schmaps/test/CampusMenuActivityTest.java
@@ -0,0 +1,17 @@
+package com.chalmers.schmaps.test;
+
+import com.chalmers.schmaps.CampusMenuActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class CampusMenuActivityTest extends ActivityInstrumentationTestCase2<CampusMenuActivity> {
+
+	public CampusMenuActivityTest() {
+		super(CampusMenuActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
